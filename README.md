1b91735ab91aaf37436169c2a4dcd94bf7e4837a
Álex Ribeiro <106709609+AlexRibeiroo@users.noreply.github.com>
2024-10-29 02:31:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-
-    return <h1>NAT, TE AMO S2</h1>
+function Home() {
+  return <h1>NAT, TE AMO S2</h1>;
 }
 
-export default Home
+export default Home;
